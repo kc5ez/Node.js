15e47f1b7d9e4e414a6512708c1695314587e68f
Ke Cheng <kichingxx@yahoo.com>
2014-04-20 12:48:39 -0400

    out of html

--- routes.js
+++ routes.js
@@ -2,10 +2,5 @@ module.exports = function(app){
     var handlers = require('./handlers');
     app.get('/', handlers.index);
 	app.get('/kecheng', handlers.kecheng);
-}
-
-module.exports = function(app){
-    var handlers = require('./handlers');
-    app.get('/', handlers.index);
 	app.get('/aboutme', handlers.aboutme);
 }
