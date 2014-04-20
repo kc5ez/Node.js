07a1fbe864e78b58336bb89ae958561f62d97e39
Ke Cheng <kichingxx@yahoo.com>
2014-04-20 12:35:17 -0400

    blah

--- handlers.js
+++ handlers.js
@@ -1,15 +1,5 @@
-var mongoose = require("mongoose")
-, Pin = mongoose.model("Pin");
-
 exports.index = function(req, res){
     res.send("Welcome to cs");
-}
-
-exports.pilates = function(req, res){
-	var video_id = req.query.v;
-	res.render('pilates', {id:video_id}, function(err, html){
-		res.send(html);	
-	})
 }
 
 exports.kecheng = function(req, res){
@@ -18,44 +8,8 @@ exports.kecheng = function(req, res){
 	})
 }
 
-exports.pinterless = function(req, res) {
-    console.log("Query: " + req.query);
-    
-    if (req.query.search) {
-	Pin.findByTitle(req.query.search, function(err, all_pins) {
-	    console.log("Pins: " + JSON.stringify(all_pins) );
-	    res.render('pinterless', { pins: all_pins }, function(err, html) {
+exports.aboutme = function(req, res){
+	res.render('aboutme', function(err, html){
 		res.send(html);
-	    })
-// res.send(JSON.stringify(result))
-	});
-
-    } else {
-	Pin.find({}, function(err, all_pins) {
-	    console.log("Pins: " + JSON.stringify(all_pins) );
-	    res.render('pinterless', { pins: all_pins }, function(err, html) {
-		res.send(html);
-	    })
-// res.send(JSON.stringify(result))
-	});
-    }
+	})
 }
-
-exports.createPin = function(req, res) {
-    var newPin = new Pin({
-	title: req.body.title,
-	description: req.body.description,
-	image_url: req.body.image_url
-    });
-
-    newPin.save(function(err) {
-	if (err) {
-	    console.log("Error saving pin");
-	} else {
-	    res.redirect('/pinterless');
-	};
-    });	
-
-}
-
-
--- server.js
+++ server.js
@@ -1,41 +1,31 @@
 var express = require("express")
-, path = require('path')
-, mongoose = require('mongoose')
-, models = require('./models');
+, path = require('path');
 
 var app = express();
 
+require('./routes')(app);
 
-
-
-
-var uristring = process.env.MONGOLAB_URI || 
-"mongodb://heroku_app22101346:bjnrfnk4kk2llmcdpl6gpeojmu@ds029277.mongolab.com:29277/heroku_app22101346"
-
+// Points Express to a folder where you keep static files
+// e.g. css or client side js files
 app.use(express.static(path.normalize(__dirname) + '/public'))
 
-app.set('views', path.normalize(__dirname) + '/public/html');
+// Tell express to use its built in error handler
+app.use(express.errorHandler());
+
+app.configure(function() {
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+})
+
+// These 3 lines tell express that we are going to be rendering html files
+// held in the public directory which should be in the same directory as this file
+app.set('views', path.normalize(__dirname) + '/public');
 app.set('view engine', 'html');
 app.engine('html', require('ejs').renderFile);
 
-app.use(express.logger('dev'));
-app.use(express.errorHandler());
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
 
-require('./routes')(app);
+var port = Number(process.env.PORT || 8000);
+app.listen(port);
+console.log("App listening on port: " + port);
 
-console.log("Trying to connect");
-mongoose.connect(uristring, function(err){
-    if (err){
-	console.log("Error: " + err
-		   ); 
-	}else{
-		       console.log("Successfully connected to mongodb at url: " + uristring);
-		       var port = Number(process.env.PORT || 8000);
-		       app.listen(port);
-		       console.log("App listening on port: " + port);
-		   }
-});
 
